545997f661ff8170fdd0f4a6caacc3dd974d7158
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:10:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   string = ""
   x = 0
   while x < array.length - 1
-    string += array[x]
+    string += array[x] + ","
     x += 1
   end
-  string += ", and " + array[array.length-1]
+  string += " and " + array[array.length-1]
   return string
 end
