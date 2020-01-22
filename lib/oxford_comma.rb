54decc681ddadcb33d7c694000f2d82321a7f66f
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:13:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  string = ""
+  
+  if array.length == 1
+    return string += array[0]
+  elsif array.length == 2
+    return string += array[0] + " and " + array[1]
+  else 
+    x = 0
+    while x < array.length - 1
+    string += array[x] + ", "
+    x += 1
+    end
+    
+    string += "and " + array[array.length-1]
+    return string
+  end
 end
