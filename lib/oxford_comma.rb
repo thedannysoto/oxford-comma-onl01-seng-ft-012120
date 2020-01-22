1cd9407162308b3acc379472edb6cfb8e700cf59
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:08:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  string = ""
+  x = 0
+  while x < array.length - 1
+    string += array[x]
+    x += 1
+  end
+  string += ", and " + array[array.length-1]
+  return string
 end
